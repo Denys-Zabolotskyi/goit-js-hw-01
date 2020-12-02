338338c2126332babcf9db149d0083e2b3251fab
Denys Zabolotskyi <d.zabolotskyi@gmail.com>
2020-12-02 02:09:46 +0200

    03 task solved

--- js/task-03.js
+++ js/task-03.js
@@ -1,33 +1,15 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
+const input = prompt('Введите пароль:');
 let message;
-const userInput = prompt('Введите пароль:');
-console.log(userInput);
 
+if (input === ADMIN_PASSWORD) {
+    message = 'Добро пожаловать!';
+}
 
-// if (ADMIN_PASSWORD === jqueryismyjam) {
-//     message = 'Добро пожаловать!'
-//     console.log(message);
-// }
-
-
-// const ADMIN_PASSWORD = prompt('Введите пароль:');
-// let message;
-
-// while (true) {
-//   if (ADMIN_PASSWORD === 'jqueryismyjam') {
-//     message = 'Добро пожаловать!';
-//     console.log(message);
-//     alert(message);
-//     break;
-//   } else if (ADMIN_PASSWORD === null) {
-//     message = 'Отменено пользователем!';
-//     console.log(message);
-//     alert(message);
-//     break;
-//   } else {
-//     message = 'Доступ запрещен, неверный пароль!';
-//     console.log(message);
-//     alert(message);
-//     break;
-//   }
-// }
+else if (input === null) {
+    message = 'Отменено пользователем!';
+}
+else {
+    message = 'Доступ запрещен, неверный пароль!';
+}
+alert(message);
