f0681ce7c3fd2bc683bbae352aaf73108a3992a4
Denys Zabolotskyi <d.zabolotskyi@gmail.com>
2020-12-02 15:25:30 +0200

    hw-01-finished

--- js/task-02.js
+++ js/task-02.js
@@ -1,7 +1,10 @@
 const total = 100;
-const ordered = 130;
-if (ordered > total) {
-console.log('"На складе недостаточно товаров!"'); 
-} else {
-    console.log('"Заказ оформлен, с вами свяжется менеджер"');
-}
+const ordered = 55;
+const message = ordered > total ?'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер!';
+console.log(message);
+
+// if (ordered > total) {
+// console.log('"На складе недостаточно товаров!"'); 
+// } else {
+//     console.log('"Заказ оформлен, с вами свяжется менеджер"');
+// }
